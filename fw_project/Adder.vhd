a61591f8c1417e9d13537274b30ad1672b326c72
LiadCohen <cohenliad1@walla.com>
2020-11-26 00:54:08 +0200

    Added 32bit register and adder (wraparound)

---
+++ fw_project/Adder.vhd
@@ -0,0 +1,16 @@
+-- LC
+-- Code your design here
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+ENTITY VectorAdder IS PORT(
+    a   		: IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    b			: IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    sum_out		: OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+);
+END VectorAdder;
+
+ARCHITECTURE Arch OF VectorAdder IS
+BEGIN
+    sum_out <= ('0' & a) + ('0' & b);
+END Arch;
---
+++ fw_project/Register.vhd
@@ -0,0 +1,37 @@
+-- LC
+-- Code your design here
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+ENTITY ShiftableRegister IS PORT(
+    input   		: IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    output  		: OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    
+    clock   		: IN STD_LOGIC;
+    clear   		: IN STD_LOGIC;
+    load    		: IN STD_LOGIC;
+    
+    shift         	: IN STD_LOGIC;
+    rotation_value 	: IN STD_LOGIC
+);
+END ShiftableRegister;
+
+ARCHITECTURE Arch OF ShiftableRegister IS
+	signal internal : STD_LOGIC_VECTOR(31 DOWNTO 0);
+BEGIN
+    process(clock, clear)
+    begin
+        if clear = '1' then
+            internal <= (others => '0');
+        elsif rising_edge(clk) then
+            if load = '1' then
+                internal <= input;
+            end if;
+            if shift = '1' then
+            	internal <= rotation_value & Dinternal(31 downto 1);
+            end if;
+        end if;
+    end process;
+    
+    output <= internal;
+END Arch;
