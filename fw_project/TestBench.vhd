213d3dc4eecae338a275a718f4e7695c7aa79bc8
LiadCohen <cohenliad1@walla.com>
2020-11-26 05:53:53 +0200

    Made the N generic to have the ability to change bit size in one place

--- fw_project/TestBench.vhd
+++ fw_project/TestBench.vhd
@@ -5,21 +5,26 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 ENTITY TestBench IS
+GENERIC(
+	N: integer := 32;
+    POW: integer := 5
+);
 END TestBench;
 
 ARCHITECTURE Arch OF TestBench IS
-    signal multiplier	: STD_LOGIC_VECTOR(31 DOWNTO 0);
-    signal multiplicand : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    signal multiplier	: STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+    signal multiplicand : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
     
     signal activate 	: std_logic := '0';
 	signal clock 		: std_logic := '0';
 	signal valid		: std_logic := '0';
     
-    signal result		: STD_LOGIC_VECTOR(63 DOWNTO 0);
+    signal result		: STD_LOGIC_VECTOR(2*N-1 DOWNTO 0);
 BEGIN
 	clock <= not clock after 20 ns;
 
 	TL_E : entity work.TopLevel(Arch)
+    	GENERIC MAP (N, POW)
     	PORT MAP (multiplier, multiplicand, result, activate, clock, valid);
     
     process
@@ -27,10 +32,10 @@ BEGIN
     	report "Start";
         
 		for i in 1 to 10 loop
-			multiplier <= std_logic_vector(to_unsigned(i,32));
+			multiplier <= std_logic_vector(to_unsigned(i,N));
 
 			for j in 1 to 10 loop
-				multiplicand <= std_logic_vector(to_unsigned(j,32));
+				multiplicand <= std_logic_vector(to_unsigned(j,N));
                 
 				activate <= '0', '1' after 5 ns, '0' after 40 ns;
 				wait for 50 ns;
