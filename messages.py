a8f887254bd2fc2455ec19a18f07f5b820906b14
almogk97 <71256952+almogk97@users.noreply.github.com>
2020-11-26 03:13:29 +0200

    Add files via upload

--- messages.py
+++ messages.py
@@ -1,23 +1,55 @@
 #!/usr/bin/env python3
 
 from hydration import *
+from enum import IntEnum
 
-class write_request(Struct):
-    opcode = UInt8(0x37)
+
+class Opcode(IntEnum):
+    WRITE_REQUEST = 0x37
+    READ_REQUEST = 0x77
+    WRITE_ACK = 0X3A
+    NACK = 0x5A
+    READ_RESPONSE = 0x7A
+
+
+def is_valid_opcode(opcode):
+    return opcode == Opcode.WRITE_REQUEST.value or opcode == Opcode.READ_REQUEST.value
+
+
+class Header(Struct):
+    opcode = UInt8
+    session = UInt8
+
+
+class WriteRequest(Struct):
+    header = Header(opcode=Opcode.WRITE_REQUEST.value)
     address = UInt32
     value = UInt32
 
-class read_request(Struct):
-    opcode = UInt8(0x77)
+
+class ReadRequest(Struct):
+    header = Header(opcode=Opcode.READ_REQUEST.value)
     address = UInt32
 
-class write_ack(Struct):
-    opcode = UInt8(0x3A)
 
-class nack(Struct):
-    opcode = UInt8(0x5A)
+class WriteAck(Struct):
+    header = Header(opcode=Opcode.WRITE_ACK.value)
+
+
+class Nack(Struct):
+    header = Header(opcode=Opcode.NACK.value)
     error_code = UInt32
 
-class read_response(Struct):
-    opcode = UInt8(0x7A)
+
+class ReadResponse(Struct):
+    header = Header(opcode=Opcode.READ_RESPONSE.value)
     value = UInt32
+
+
+lengths = {
+        Opcode.WRITE_REQUEST.value: len(WriteRequest),
+        Opcode.READ_REQUEST.value: len(ReadRequest),
+        Opcode.WRITE_ACK.value: len(WriteAck),
+        Opcode.NACK.value: len(Nack),
+        Opcode.READ_RESPONSE.value: len(ReadResponse)
+}
--- reg-like.py
+++ reg-like.py
@@ -1,85 +1,101 @@
 #!/usr/bin/env python3
 
-from hydration import *
-from multiprocessing import Pool
-import threading, queue
-
-import select
+import threading
+import queue
+from messages import *
 import socket
-import sys
-import Queue
+
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+NUM_THREADS = 3
 
-handlers = {0x37: write_request_handler, 0x77: read_request_handler,
-            0x3A: write_ack_handler, 0x7A: read_response}
-
-class write_request(Struct):
-    opcode = UInt8(0x37)
-    address = UInt32
-    value = UInt32
-
-class read_request(Struct):
-    opcode = UInt8(0x77)
-    address = UInt32
-
-class write_ack(Struct):
-    opcode = UInt8(0x3A)
-
-class nack(Struct):
-    opcode = UInt8(0x5A)
-    error_code = UInt32
-
-class read_response(Struct):
-    opcode = UInt8(0x7A)
-    value = UInt32
+requests_queue = queue.Queue()
+responses_queue = queue.Queue()
 
 
-def thread_read_for_cpp():
-    # listen on socket to register_message
-    # read from what vhdl give us
-    reg_value = address # just for mock
-    thread_write_for_cpp(reg_value)
+def write_to_vhdl(address, value):
+    # send to vhdl - YUVAL IMPLEMENTATION
+    print("wrote")
+    return True or (address - value)
 
 
-def thread_write_for_cpp(value):
-    # write on socket
-    return 1
-
-def thread_read_for_vhdl():
-    return 1
+def read_from_vhdl(address):
+    # YUVAL IMPLEMENTATION
+    print("read")
+    return address - address
 
 
-def send_to_vhdl():
-    # send to vhdl
-    return 1
+def send_nack(session, error_code):
+    n = Nack(header=Header(session=session), error_code=error_code)
+    responses_queue.put(n.serialize())
+    print("sent nack with error code: " + str(error_code) + " and session " + str(session))
 
-def read_from_vhdl():
-    reg_value = 0 # just for mock
-    return reg_value
+
+def handle_read_request(msg):
+    m = ReadRequest.from_bytes(msg)
+    value = read_from_vhdl(m.address)
+    response = ReadResponse(header=Header(session=m.header.session), value=value)
+    responses_queue.put(response.serialize())
+
+
+# TODO edit this later
+WRITE_FAIL = 0x13
+
+
+def handle_write_request(msg):
+    m = WriteRequest.from_bytes(msg)
+    write_status = write_to_vhdl(m.address, m.value)
+    if write_status:
+        response = WriteAck(header=Header(session=m.header.session))
+        responses_queue.put(response.serialize())
+    else:
+        send_nack(m.header.session, WRITE_FAIL)
+
+
+def worker():
+    while True:
+        msg = requests_queue.get()
+        if msg is None:
+            continue
+        opcode = msg[0]
+        if opcode == Opcode.READ_REQUEST.value:
+            handle_read_request(msg)
+        else:
+            handle_write_request(msg)
+
 
 def main():
     # create socket
+    threads = []
+    for i in range(NUM_THREADS):
+        threads.append(threading.Thread(target=worker, daemon=True))
+        threads[i].start()
 
-    # queue
-    q = queue.Queue()
-
-
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         conn, addr = s.accept()
         with conn:
             print('Connected by', addr)
-            while True:
-                opcode = conn.recv(1)
-                if not opcode:
-                    break
-                handlers[opcode[0]](conn)
+            try:
+                while True:
+                    opcode = conn.recv(1)
+                    if not opcode:
+                        continue
+                    if not is_valid_opcode(opcode[0]):
+                        # TODO add session and error code and make sure to flush the conn
+                        send_nack(0, 1)
+                        continue
+                    rest_of_msg = conn.recv(lengths[opcode[0]] - 1)
+                    requests_queue.put(opcode + rest_of_msg)
 
-                conn.sendall(data)
+                    response = responses_queue.get()
+                    if response is not None:
+                        conn.sendall(response)
+            except ConnectionResetError:
+                print("connection is over")
         
 
 if __name__ == '__main__':
+    main()
